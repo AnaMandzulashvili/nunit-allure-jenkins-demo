029b00fa40a2c31af6ff0ffbd896c50e2cb5c1b2
Anna Mandzulashvili <anna.mandzulshvili@betfred.com>
2025-12-09 15:37:05 +0400

    change test

--- AllureJenkinsGit/CalculatorTests.cs
+++ AllureJenkinsGit/CalculatorTests.cs
@@ -1,26 +1,51 @@
-using NUnit.Allure.Core;
-using NUnit.Allure.Attributes;
-
+using NUnit.Framework;
+using Allure.Net.Commons;
+using Allure.NUnit.Attributes;
 
 namespace AllureJenkinsGit
 {
-   
     [TestFixture]
     [AllureSuite("Calculator Tests")]
-    // Remove [AllureDisplayIgnored] from here
     public class CalculatorTests
     {
         [Test]
         [AllureTag("Regression")]
         [AllureSubSuite("Addition")]
         [AllureOwner("Ana")]
+        [Description("Test to verify addition of two numbers")]
         public void AddTest()
         {
-            int result = Add(2, 3);
-            Assert.AreEqual(5, result);
+            int firstNumber = SetFirstNumber(2);
+            int secondNumber = SetSecondNumber(3);
+            int result = PerformAddition(firstNumber, secondNumber);
+            VerifyResult(result, 5);
         }
 
-        public int Add(int a, int b)
+        [AllureStep("Set first number to {number}")]
+        private int SetFirstNumber(int number)
+        {
+            return number;
+        }
+
+        [AllureStep("Set second number to {number}")]
+        private int SetSecondNumber(int number)
+        {
+            return number;
+        }
+
+        [AllureStep("Add {a} and {b}")]
+        private int PerformAddition(int a, int b)
+        {
+            return Add(a, b);
+        }
+
+        [AllureStep("Verify result is {expected}")]
+        private void VerifyResult(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        private int Add(int a, int b)
         {
             return a + b;
         }
